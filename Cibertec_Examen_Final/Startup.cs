34608f1158dccccfdb6b7fcabfcfa40e5e2ce8ee
StephMachado <steph_machado@hotmail.com>
2016-08-15 19:19:56 -0500

    Creación de proyecto

---
+++ Cibertec_Examen_Final/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Cibertec_Examen_Final.Startup))]
+namespace Cibertec_Examen_Final
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
